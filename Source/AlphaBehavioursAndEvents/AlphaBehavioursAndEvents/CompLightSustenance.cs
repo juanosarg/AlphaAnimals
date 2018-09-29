ed35457adff7651b5455df25f0a475d36ca1f8fa
juanosarg <31105173+juanosarg@users.noreply.github.com>
2018-09-29 19:58:51 +0200

    v0.1

---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/CompLightSustenance.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompLightSustenance : ThingComp
+    {
+
+
+        public float growOptimalGlow = 0.4f;
+        private bool addHediffOnce = true;
+
+
+
+        public CompProperties_LightSustenance Props
+        {
+            get
+            {
+                return (CompProperties_LightSustenance)this.props;
+            }
+        }
+
+
+        public override void CompTickRare()
+        {
+            Pawn pawn = this.parent as Pawn;
+            if (addHediffOnce)
+            {
+                pawn.health.AddHediff(HediffDef.Named("GR_LightSustenance"));
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("GR_LightSustenance"), false);
+                hediff.Severity = 0.0f;
+                addHediffOnce = false;
+            }
+            float num = this.parent.Map.glowGrid.GameGlowAt(this.parent.Position, false);
+            //Log.Warning("Light level "+num.ToString());
+
+            if (num >= growOptimalGlow)
+            {
+
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("GR_LightSustenance"), false);
+
+                if ((hediff != null) && hediff.Severity > 0f)
+                {
+                    hediff.Severity -= 0.005f;
+                    //Log.Warning("Severity " + hediff.Severity.ToString());
+                }
+            }
+            else
+            {
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("GR_LightSustenance"), false);
+
+                if ((hediff != null) && hediff.Severity < 1f)
+                {
+
+                    hediff.Severity += 0.005f;
+                    // Log.Warning("Severity " + hediff.Severity.ToString());
+
+                }
+            }
+        }
+
+
+    }
+}
+
