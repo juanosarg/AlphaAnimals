0c7b514743503036fc90066624205ae687219fe4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2018-10-04 17:02:29 +0200

    v0.1

---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/CompHighlyFlammable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompHighlyFlammable : ThingComp
+    {
+
+      
+
+
+
+
+        public CompProperties_HighlyFlammable Props
+        {
+            get
+            {
+                return (CompProperties_HighlyFlammable)this.props;
+            }
+        }
+
+       
+
+
+    }
+}
