02e87c817c6912f73ca512e3ef2e58514edada54
sargbjornson <sarg.auxiliar@gmail.com>
2018-11-30 23:38:39 +0100

    v1.09

---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/CompFertilizer.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompFertilizer : ThingComp
+    {
+
+
+      
+
+
+
+        public CompProperties_Fertilizer Props
+        {
+            get
+            {
+                return (CompProperties_Fertilizer)this.props;
+            }
+        }
+
+
+        public override void CompTick()
+        {
+            Pawn pawn = this.parent as Pawn;
+
+            if (pawn.Spawned)
+            {
+              if (pawn.Position.GetTerrain(pawn.Map) == TerrainDef.Named("Sand"))
+                {
+                    pawn.Map.terrainGrid.SetTerrain(pawn.Position, TerrainDef.Named("AA_FertilizedSand"));
+                }
+                
+
+            }
+        }
+
+
+    }
+}
+
