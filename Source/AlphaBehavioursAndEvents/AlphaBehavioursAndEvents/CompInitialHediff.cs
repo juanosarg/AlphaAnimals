e4cbbee6714f25fc8a8cd813380cb82c24dbb6b9
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-04-05 19:56:41 +0200

    v1.2

---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/CompInitialHediff.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+using Verse;
+using System.Linq;
+
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompInitialHediff : ThingComp
+    {
+        private bool addHediffOnce = true;
+
+
+
+        public CompProperties_InitialHediff Props
+        {
+            get
+            {
+                return (CompProperties_InitialHediff)this.props;
+            }
+        }
+
+
+
+
+        public override void CompTickRare()
+        {
+
+            base.CompTickRare();
+
+            if (addHediffOnce)
+            {
+                //Log.Message("Ticking");
+                Pawn pawn = this.parent as Pawn;
+                pawn.health.AddHediff(HediffDef.Named(Props.hediffname));
+                //Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named(Props.hediffname), false);
+                //hediff.Severity = Props.hediffseverity;
+                addHediffOnce = false;
+            }
+        }
+
+
+    }
+}
---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/CompProperties_InitialHediff.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompProperties_InitialHediff : CompProperties
+    {
+
+        public string hediffname = "";
+        public float hediffseverity = 0f;
+
+
+
+
+        public CompProperties_InitialHediff()
+        {
+            this.compClass = typeof(CompInitialHediff);
+        }
+
+
+    }
+}
