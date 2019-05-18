1cb86e4383f4dd38babe8d2ce7a71f7dc39499b6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-05-18 15:54:50 +0200

    v1.4

--- Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/AlphaAnimals_Settings.cs
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/AlphaAnimals_Settings.cs
@@ -8,8 +8,8 @@ namespace AlphaBehavioursAndEvents
     class AlphaAnimals_Settings: ModSettings
         
     {
+           
 
-       
             public bool flagBlackHiveRaids = true;
             public bool flagStalkingLions = true;
             public bool flagCactipineDroppods = true;
@@ -21,14 +21,45 @@ namespace AlphaBehavioursAndEvents
             public bool flagFrostmites = true;
             public bool flagAsteroids = true;
 
-
-
-
-
-
-
-
-
+            public bool AA_AerofleetFlag = false;
+            public bool AA_AnimusVoxFlag = false;
+            public bool AA_ArcticLionFlag = false;
+            public bool AA_BarbslingerFlag = false;
+            public bool AA_BedBugFlag = false;
+            public bool AA_BlizzariskFlag = false;
+            public bool AA_BoulderMitFlag = false;
+            public bool AA_BumbledroneFlag = false;
+            public bool AA_CactipineFlag = false;
+            public bool AA_DecayDrakeFlag = false;
+            public bool AA_DunealiskFlag = false;
+            public bool AA_FeraliskFlag = false;
+            public bool AA_FissionMouseFlag = false;
+            public bool AA_FrostmiteFlag = false;
+            public bool AA_GigantelopeFlag = false;
+            public bool AA_GreatDevourerFlag = false;
+            public bool AA_GroundrunnerFlag = false;
+            public bool AA_LockjawFlag = false;
+            public bool AA_MammothWormFlag = false;
+            public bool AA_MantrapFlag = false;
+            public bool AA_MeadowAveFlag = false;
+            public bool AA_MegaLouseFlag = false;
+            public bool AA_NeedlepostFlag = false;
+            public bool AA_NeedlerollFlag = false;
+            public bool AA_NightlingFlag = false;
+            public bool AA_OcularJellyFlag = false;
+            public bool AA_OvergrownColossusFlag = false;
+            public bool AA_PebbleMitFlag = false;
+            public bool AA_RadyakFlag = false;
+            public bool AA_RaptorShrimpFlag = false;
+            public bool AA_SandLionFlag = false;
+            public bool AA_SandSquidFlag = false;
+            public bool AA_ShockGoatFlag = false;
+            public bool AA_SlurrypedeFlag = false;
+            public bool AA_TetraSlugFlag = false;
+            public bool AA_ThermadonFlag = false;
+            public bool AA_WildpawnFlag = false;
+            public bool AA_WildpodFlag = false;
+           
 
         public override void ExposeData()
             {
@@ -44,6 +75,47 @@ namespace AlphaBehavioursAndEvents
                 Scribe_Values.Look(ref this.flagFrostmites, "flagFrostmites", true);
                 Scribe_Values.Look(ref this.flagAsteroids, "flagAsteroid", true);
 
+                Scribe_Values.Look(ref this.AA_AerofleetFlag, "AA_AerofleetFlag", false);
+                Scribe_Values.Look(ref this.AA_AnimusVoxFlag, "AA_AnimusVoxFlag", false);
+                Scribe_Values.Look(ref this.AA_ArcticLionFlag, "AA_ArcticLionFlag", false);
+                Scribe_Values.Look(ref this.AA_BarbslingerFlag, "AA_BarbslingerFlag", false);
+                Scribe_Values.Look(ref this.AA_BedBugFlag, "AA_BedBugFlag", false);
+                Scribe_Values.Look(ref this.AA_BlizzariskFlag, "AA_BlizzariskFlag", false);
+                Scribe_Values.Look(ref this.AA_BoulderMitFlag, "AA_BoulderMitFlag", false);
+                Scribe_Values.Look(ref this.AA_BumbledroneFlag, "AA_BumbledroneFlag", false);
+                Scribe_Values.Look(ref this.AA_CactipineFlag, "AA_CactipineFlag", false);
+                Scribe_Values.Look(ref this.AA_DecayDrakeFlag, "AA_DecayDrakeFlag", false);
+                Scribe_Values.Look(ref this.AA_DunealiskFlag, "AA_DunealiskFlag", false);
+                Scribe_Values.Look(ref this.AA_FeraliskFlag, "AA_FeraliskFlag", false);
+                Scribe_Values.Look(ref this.AA_FissionMouseFlag, "AA_FissionMouseFlag", false);
+                Scribe_Values.Look(ref this.AA_FrostmiteFlag, "AA_FrostmiteFlag", false);
+                Scribe_Values.Look(ref this.AA_GigantelopeFlag, "AA_GigantelopeFlag", false);
+                Scribe_Values.Look(ref this.AA_GreatDevourerFlag, "AA_GreatDevourerFlag", false);
+                Scribe_Values.Look(ref this.AA_GroundrunnerFlag, "AA_GroundrunnerFlag", false);
+                Scribe_Values.Look(ref this.AA_LockjawFlag, "AA_LockjawFlag", false);
+                Scribe_Values.Look(ref this.AA_MammothWormFlag, "AA_MammothWormFlag", false);
+                Scribe_Values.Look(ref this.AA_MantrapFlag, "AA_MantrapFlag", false);
+                Scribe_Values.Look(ref this.AA_MeadowAveFlag, "AA_MeadowAveFlag", false);
+                Scribe_Values.Look(ref this.AA_MegaLouseFlag, "AA_MegaLouseFlag", false);
+                Scribe_Values.Look(ref this.AA_NeedlepostFlag, "AA_NeedlepostFlag", false);
+                Scribe_Values.Look(ref this.AA_NeedlerollFlag, "AA_NeedlerollFlag", false);
+                Scribe_Values.Look(ref this.AA_NightlingFlag, "AA_NightlingFlag", false);
+                Scribe_Values.Look(ref this.AA_OcularJellyFlag, "AA_OcularJellyFlag", false);
+                Scribe_Values.Look(ref this.AA_OvergrownColossusFlag, "AA_OvergrownColossusFlag", false);
+                Scribe_Values.Look(ref this.AA_PebbleMitFlag, "AA_PebbleMitFlag", false);
+                Scribe_Values.Look(ref this.AA_RadyakFlag, "AA_RadyakFlag", false);
+                Scribe_Values.Look(ref this.AA_RaptorShrimpFlag, "AA_RaptorShrimpFlag", false);
+                Scribe_Values.Look(ref this.AA_SandLionFlag, "AA_SandLionFlag", false);
+                Scribe_Values.Look(ref this.AA_SandSquidFlag, "AA_SandSquidFlag", false);
+                Scribe_Values.Look(ref this.AA_ShockGoatFlag, "AA_ShockGoatFlag", false);
+                Scribe_Values.Look(ref this.AA_SlurrypedeFlag, "AA_SlurrypedeFlag", false);
+                Scribe_Values.Look(ref this.AA_TetraSlugFlag, "AA_TetraSlugFlag", false);
+                Scribe_Values.Look(ref this.AA_ThermadonFlag, "AA_ThermadonFlag", false);
+                Scribe_Values.Look(ref this.AA_WildpawnFlag, "AA_WildpawnFlag", false);
+                Scribe_Values.Look(ref this.AA_WildpodFlag, "AA_WildpodFlag", false);
+
+
+
 
 
 
@@ -52,18 +124,22 @@ namespace AlphaBehavioursAndEvents
 
 
     }
-    class AlphaAnimals_Mod : Mod
+
+    class AlphaAnimalsEvents_Mod : Mod
     {
+
         public static AlphaAnimals_Settings settings;
-        public AlphaAnimals_Mod(ModContentPack content) : base(content)
+        public AlphaAnimalsEvents_Mod(ModContentPack content) : base(content)
         {
             settings = GetSettings<AlphaAnimals_Settings>();
         }
-        public override string SettingsCategory() => "Alpha Animals";
+        public override string SettingsCategory() => "Alpha Animals, Events";
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
             Listing_Standard ls = new Listing_Standard();
+
+
             ls.Begin(inRect);
             ls.Gap(12f);
             ls.CheckboxLabeled("allowBlackHive".Translate(), ref settings.flagBlackHiveRaids, null);
@@ -85,7 +161,112 @@ namespace AlphaBehavioursAndEvents
             ls.CheckboxLabeled("allowFrostmiteCorpses".Translate(), ref settings.flagFrostmites, null);
             ls.Gap(12f);
             ls.CheckboxLabeled("allowAsteroids".Translate(), ref settings.flagAsteroids, null);
-            ls.Gap(12f);
+           
+            settings.Write();
+            ls.End();
+
+
+        }
+    }
+
+    class AlphaAnimals_Mod : Mod
+    {
+      
+        public static AlphaAnimals_Settings settings;
+        public AlphaAnimals_Mod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<AlphaAnimals_Settings>();
+        }
+        public override string SettingsCategory() => "Alpha Animals";
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+
+           
+            ls.Begin(inRect);
+            ls.ColumnWidth = inRect.width / 2.05f;
+           
+            ls.CheckboxLabeled("disableAerofleet".Translate(), ref settings.AA_AerofleetFlag, null);
+           
+            ls.CheckboxLabeled("disableAnimusVox".Translate(), ref settings.AA_AnimusVoxFlag, null);
+           
+            ls.CheckboxLabeled("disableArcticLion".Translate(), ref settings.AA_ArcticLionFlag, null);
+         
+            ls.CheckboxLabeled("disableBarbslinger".Translate(), ref settings.AA_BarbslingerFlag, null);
+           
+            ls.CheckboxLabeled("disableBedBug".Translate(), ref settings.AA_BedBugFlag, null);
+           
+            ls.CheckboxLabeled("disableBlizzarisk".Translate(), ref settings.AA_BlizzariskFlag, null);
+            
+            ls.CheckboxLabeled("disableBoulderMit".Translate(), ref settings.AA_BoulderMitFlag, null);
+            
+            ls.CheckboxLabeled("disableBumbledrone".Translate(), ref settings.AA_BumbledroneFlag, null);
+           
+            ls.CheckboxLabeled("disableCactipine".Translate(), ref settings.AA_CactipineFlag, null);
+           
+            ls.CheckboxLabeled("disableDecayDrake".Translate(), ref settings.AA_DecayDrakeFlag, null);
+           
+            ls.CheckboxLabeled("disableDunealisk".Translate(), ref settings.AA_DunealiskFlag, null);
+            
+            ls.CheckboxLabeled("disableFeralisk".Translate(), ref settings.AA_FeraliskFlag, null);
+           
+            ls.CheckboxLabeled("disableFissionMouse".Translate(), ref settings.AA_FissionMouseFlag, null);
+            
+            ls.CheckboxLabeled("disableFrostmite".Translate(), ref settings.AA_FrostmiteFlag, null);
+           
+            ls.CheckboxLabeled("disableGigantelope".Translate(), ref settings.AA_GigantelopeFlag, null);
+            
+            ls.CheckboxLabeled("disableGreatDevourer".Translate(), ref settings.AA_GreatDevourerFlag, null);
+           
+            ls.CheckboxLabeled("disableGroundrunner".Translate(), ref settings.AA_GroundrunnerFlag, null);
+            
+            ls.CheckboxLabeled("disableLockjaw".Translate(), ref settings.AA_LockjawFlag, null);
+         
+            ls.CheckboxLabeled("disableMammothWorm".Translate(), ref settings.AA_MammothWormFlag, null);
+           
+            ls.CheckboxLabeled("disableMantrap".Translate(), ref settings.AA_MantrapFlag, null);
+            ls.NewColumn();
+            ls.CheckboxLabeled("disableMeadowAve".Translate(), ref settings.AA_MeadowAveFlag, null);
+           
+            ls.CheckboxLabeled("disableMegaLouse".Translate(), ref settings.AA_MegaLouseFlag, null);
+           
+            ls.CheckboxLabeled("disableNeedlepost".Translate(), ref settings.AA_NeedlepostFlag, null);
+            
+            ls.CheckboxLabeled("disableNeedleroll".Translate(), ref settings.AA_NeedlerollFlag, null);
+            
+            ls.CheckboxLabeled("disableNightling".Translate(), ref settings.AA_NightlingFlag, null);
+         
+            ls.CheckboxLabeled("disableOcularJelly".Translate(), ref settings.AA_OcularJellyFlag, null);
+           
+            ls.CheckboxLabeled("disableOvergrownColossus".Translate(), ref settings.AA_OvergrownColossusFlag, null);
+           
+            ls.CheckboxLabeled("disablePebbleMit".Translate(), ref settings.AA_PebbleMitFlag, null);
+           
+            ls.CheckboxLabeled("disableRadyak".Translate(), ref settings.AA_RadyakFlag, null);
+            
+            ls.CheckboxLabeled("disableRaptorShrimp".Translate(), ref settings.AA_RaptorShrimpFlag, null);
+            
+            ls.CheckboxLabeled("disableSandLion".Translate(), ref settings.AA_SandLionFlag, null);
+         
+            ls.CheckboxLabeled("disableSandSquid".Translate(), ref settings.AA_SandSquidFlag, null);
+            
+            ls.CheckboxLabeled("disableShockGoat".Translate(), ref settings.AA_ShockGoatFlag, null);
+           
+            ls.CheckboxLabeled("disableSlurrypede".Translate(), ref settings.AA_SlurrypedeFlag, null);
+            
+            ls.CheckboxLabeled("disableTetraSlug".Translate(), ref settings.AA_TetraSlugFlag, null);
+            
+            ls.CheckboxLabeled("disableThermadon".Translate(), ref settings.AA_ThermadonFlag, null);
+           
+            ls.CheckboxLabeled("disableWildpawn".Translate(), ref settings.AA_WildpawnFlag, null);
+        
+            ls.CheckboxLabeled("disableWildpod".Translate(), ref settings.AA_WildpodFlag, null);
+           
+
+
+
+
             settings.Write();
             ls.End();
 
---
+++ Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/HarmonyPatchers.cs
@@ -0,0 +1,275 @@
+﻿using Harmony;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Verse.AI;
+using RimWorld.Planet;
+
+
+// So, let's comment this code, since it uses Harmony and has moderate complexity
+
+namespace AlphaBehavioursAndEvents
+{
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = HarmonyInstance.Create("com.alphaanimals");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+  
+
+    /*This Harmony Prefix makes the creature carry more weight
+    */
+    [HarmonyPatch(typeof(MassUtility))]
+    [HarmonyPatch("Capacity")]
+    public static class MassUtility_Capacity_Patch
+    {
+        [HarmonyPostfix]
+        public static void MakeGigantelopesCarryMore(Pawn p, ref float __result)
+
+        {
+            bool flagIsCreatureMine = p.Faction != null && p.Faction.IsPlayer;
+            bool flagDoesCreatureHaveTheHediffs = (p.TryGetComp<CompInitialHediff>() != null);
+            bool flagCanCreatureCarryMore = false;
+            if (flagDoesCreatureHaveTheHediffs)
+            {
+                flagCanCreatureCarryMore = (p.TryGetComp<CompInitialHediff>().Props.hediffname == "AA_CarryWeight");
+            }
+            
+
+            
+                               
+
+            if (flagIsCreatureMine && flagCanCreatureCarryMore)
+            {
+                int factor = p.TryGetComp<CompInitialHediff>().phase;
+                __result = p.BodySize * 35f + factor*factor;
+            }
+
+        }
+    }
+
+    /*Buckle your belts or something, we are doing Transpilers!*/
+
+    [HarmonyPatch(typeof(WildAnimalSpawner))]
+    [HarmonyPatch("SpawnRandomWildAnimalAt")]
+    public static class WildAnimalSpawner_SpawnRandomWildAnimalAt_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            Label label = ilg.DefineLabel();
+            int i = 0;
+            foreach (CodeInstruction instruction in codes)
+            {
+                if (instruction.opcode == OpCodes.Stloc_0)
+                {
+                    codes[i + 1].labels.Add(label);
+                    yield return new CodeInstruction(OpCodes.Stloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);//Load "PawnKindDef" local variable. 
+                    yield return new CodeInstruction(OpCodes.Call, typeof(WildAnimalSpawner_SpawnRandomWildAnimalAt_Patch).GetMethod("DetectAlphaCreatureAndOptions"));
+                    yield return new CodeInstruction(OpCodes.Brfalse, label);
+                    yield return new CodeInstruction(OpCodes.Ret);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+
+                i++;
+            }
+
+        }
+
+        public static bool DetectAlphaCreatureAndOptions(PawnKindDef theCreature)
+        {
+            if (theCreature != null)
+            {
+                //Log.Message("Creature that was going to spawn was" + theCreature.defName);
+                
+                if (theCreature.defName.Contains("AA_"))
+                {
+                    
+                    if (theCreature.defName.Contains("AA_Aerofleet") &&LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_AerofleetFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_AnimusVox") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_AnimusVoxFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_ArcticLion") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_ArcticLionFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Barbslinger") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_BarbslingerFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_BedBug") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_BedBugFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Blizzarisk") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_BlizzariskFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_BoulderMit") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_BoulderMitFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Bumbledrone") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_BumbledroneFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Cactipine") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_CactipineFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_DecayDrake") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_DecayDrakeFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Dunealisk") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_DunealiskFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Feralisk") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_FeraliskFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_FissionMouse") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_FissionMouseFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Frostmite") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_FrostmiteFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Gigantelope") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_GigantelopeFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_GreatDevourer") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_GreatDevourerFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Groundrunner") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_GroundrunnerFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Lockjaw") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_LockjawFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_MammothWorm") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_MammothWormFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Mantrap") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_MantrapFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_MeadowAve") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_MeadowAveFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_MegaLouse") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_MegaLouseFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Needlepost") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_NeedlepostFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Needleroll") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_NeedlerollFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Nightling") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_NightlingFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_OcularJelly") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_OcularJellyFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_OvergrownColossus") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_OvergrownColossusFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_PebbleMit") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_PebbleMitFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Radyak") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_RadyakFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_RaptorShrimp") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_RaptorShrimpFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_SandLion") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_SandLionFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_SandSquid") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_SandSquidFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_ShockGoat") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_ShockGoatFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Slurrypede") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_SlurrypedeFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_TetraSlug") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_TetraSlugFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Thermadon") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_ThermadonFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Wildpawn") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_WildpawnFlag)
+                    {
+                        return true;
+                    }
+                    if (theCreature.defName.Contains("AA_Wildpod") && LoadedModManager.GetMod<AlphaAnimals_Mod>().GetSettings<AlphaAnimals_Settings>().AA_WildpodFlag)
+                    {
+                        return true;
+                    }
+
+
+                    return false;
+                    
+                } else return false;
+            }
+            else return false;
+            
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
