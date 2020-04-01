699ef55825b5dca59f5a891a77cbd10deb97bf6c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-04-01 12:07:22 +0200

    v2.201

--- 1.1/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompCorpseDecayer.cs
+++ 1.1/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompCorpseDecayer.cs
@@ -51,28 +51,33 @@ namespace AlphaBehavioursAndEvents
                                     Corpse corpse = thingInCell as Corpse;
                                     if (corpse != null)
                                     {
-                                        corpse.HitPoints -= 5;
-                                        pawn.needs.food.CurLevel += Props.nutritionGained;
-                                       
-                                        CompRottable compRottable = corpse.TryGetComp<CompRottable>();
-                                        if (compRottable.Stage == RotStage.Fresh) {
-                                            compRottable.RotProgress += 100000;
-                                        }
-                                            
-                                        if (corpse.HitPoints < 0)
-                                        {
-                                            corpse.Destroy(DestroyMode.Vanish);
-                                            for (int i = 0; i < 20; i++)
-                                            {
-                                                IntVec3 c;
-                                                CellFinder.TryFindRandomReachableCellNear(pawn.Position,pawn.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
-                                                FilthMaker.TryMakeFilth(c, pawn.Map, ThingDefOf.Filth_CorpseBile,pawn.LabelIndefinite(), 1, FilthSourceFlags.None);
-                                                SoundDef.Named(Props.corpseSound).PlayOneShot(new TargetInfo(pawn.Position, pawn.Map, false));
+                                        if (corpse.InnerPawn.def.race.IsFlesh) {
+                                            corpse.HitPoints -= 5;
+                                            pawn.needs.food.CurLevel += Props.nutritionGained;
 
+                                            CompRottable compRottable = corpse.TryGetComp<CompRottable>();
+                                            if (compRottable.Stage == RotStage.Fresh)
+                                            {
+                                                compRottable.RotProgress += 100000;
                                             }
+
+                                            if (corpse.HitPoints < 0)
+                                            {
+                                                corpse.Destroy(DestroyMode.Vanish);
+                                                for (int i = 0; i < 20; i++)
+                                                {
+                                                    IntVec3 c;
+                                                    CellFinder.TryFindRandomReachableCellNear(pawn.Position, pawn.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                                                    FilthMaker.TryMakeFilth(c, pawn.Map, ThingDefOf.Filth_CorpseBile, pawn.LabelIndefinite(), 1, FilthSourceFlags.None);
+                                                    SoundDef.Named(Props.corpseSound).PlayOneShot(new TargetInfo(pawn.Position, pawn.Map, false));
+
+                                                }
+                                            }
+                                            FilthMaker.TryMakeFilth(current, pawn.Map, ThingDefOf.Filth_CorpseBile, pawn.LabelIndefinite(), 1, FilthSourceFlags.None);
+                                            flagOnce = true;
+
                                         }
-                                        FilthMaker.TryMakeFilth(current, pawn.Map, ThingDefOf.Filth_CorpseBile, pawn.LabelIndefinite(), 1, FilthSourceFlags.None);
-                                        flagOnce = true;
+                                        
                                     }
 
                                 }
