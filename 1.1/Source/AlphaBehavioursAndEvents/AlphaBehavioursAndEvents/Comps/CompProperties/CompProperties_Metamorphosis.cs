e4b5dbe5e1c6146089db28e6ee7bd19c474ff6b1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-04-24 18:09:57 +0200

    v2.3

---
+++ 1.1/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompProperties/CompProperties_Metamorphosis.cs
@@ -0,0 +1,18 @@
+﻿
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompProperties_Metamorphosis : CompProperties
+    {
+
+        public float timeInYears;
+        public string pawnToTurnInto;
+
+        public CompProperties_Metamorphosis()
+        {
+            this.compClass = typeof(CompMetamorphosis);
+        }
+
+    }
+}
