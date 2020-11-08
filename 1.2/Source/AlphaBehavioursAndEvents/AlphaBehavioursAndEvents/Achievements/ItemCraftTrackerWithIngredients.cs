adce9242f87707f29f9b9397defba4f9a275e9ce
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-11-08 09:12:50 +0100

    v2.802

--- 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Achievements/ItemCraftTrackerWithIngredients.cs
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Achievements/ItemCraftTrackerWithIngredients.cs
@@ -6,44 +6,68 @@ using RimWorld;
 
 namespace AchievementsExpanded
 {
-    public class ItemCraftTrackerWithIngredients:ItemCraftTracker
+    public class ItemCraftTrackerWithIngredients : Tracker<Thing>
     {
-       
+
+        public override string Key => "ItemCraftTrackerWithIngredients";
+
+        public override MethodInfo MethodHook => AccessTools.Method(typeof(QuestManager), nameof(QuestManager.Notify_ThingsProduced));
+        public override MethodInfo PatchMethod => AccessTools.Method(typeof(QuestManager_Notify_ThingsProduced_Patch), nameof(QuestManager_Notify_ThingsProduced_Patch.CheckItemCraftedIngredients));
+        protected override string[] DebugText => new string[] { $"Def: {def?.defName ?? "None"}",
+                                                                $"MadeFrom: {madeFrom?.defName ?? "Any"}",
+                                                                $"Quality: {quality}",
+                                                                $"Count: {count}",
+                                                                $"Current: {triggeredCount}" };
+
         public ItemCraftTrackerWithIngredients()
         {
         }
 
         public ItemCraftTrackerWithIngredients(ItemCraftTrackerWithIngredients reference) : base(reference)
         {
+            def = reference.def;
+            madeFrom = reference.madeFrom;
+            quality = reference.quality;
+            count = reference.count;
+            triggeredCount = 0;
             includeingredient = reference.includeingredient;
-           
+
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
-           
-            Scribe_Values.Look(ref includeingredient, "includeingredient");
-          
+            Scribe_Defs.Look(ref def, "def");
+            Scribe_Defs.Look(ref madeFrom, "madeFrom");
+            Scribe_Values.Look(ref quality, "quality");
+            Scribe_Values.Look(ref count, "count");
+            Scribe_Values.Look(ref triggeredCount, "triggeredCount");
+            Scribe_Defs.Look(ref includeingredient, "includeingredient");
+
         }
 
         public override bool Trigger(Thing thing)
         {
-           // base.Trigger(thing);
+            base.Trigger(thing);
             if ((def is null || thing.def == def) && (madeFrom is null || madeFrom == thing.Stuff)
                 && (includeingredient is null || (thing.TryGetComp<CompIngredients>().ingredients.Contains(includeingredient)))
                 )
             {
                 if (quality is null || (thing.TryGetQuality(out var qc) && qc >= quality))
                 {
-                    triggeredCount++;
+                    triggeredCount = triggeredCount + thing.stackCount;
+
                 }
             }
             return triggeredCount >= count;
         }
 
         public ThingDef includeingredient;
-      
+        public ThingDef def;
+        public ThingDef madeFrom;
+        public QualityCategory? quality;
+        public int count;
+
         private int triggeredCount;
     }
 }
---
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Harmony/QuestManager_Notify_ThingsProduced_Patch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace AchievementsExpanded
+{
+    public class QuestManager_Notify_ThingsProduced_Patch
+    {
+
+        public static void CheckItemCraftedIngredients(Pawn worker, List<Thing> things)
+        {
+            foreach (var card in AchievementPointManager.GetCards<ItemCraftTrackerWithIngredients>())
+            {
+                foreach (Thing thing in things)
+                {
+                    if ((card.tracker as ItemCraftTrackerWithIngredients).Trigger(thing))
+                    {
+                        card.UnlockCard();
+                    }
+                }
+            }
+        }
+    }
+}
