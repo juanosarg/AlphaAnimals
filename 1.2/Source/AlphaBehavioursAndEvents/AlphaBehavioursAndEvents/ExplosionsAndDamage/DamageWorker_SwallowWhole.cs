ea55a7e6440ae36d0ca95bbf8a65578552ac0d6b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-12-01 16:42:18 +0100

    v2.904

---
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/ExplosionsAndDamage/DamageWorker_SwallowWhole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+
+    public class DamageWorker_SwallowWhole : DamageWorker_Cut
+    {
+
+
+        protected override void ApplySpecialEffectsToPart(Pawn pawn, float totalDamage, DamageInfo dinfo, DamageWorker.DamageResult result)
+        {
+            base.ApplySpecialEffectsToPart(pawn, totalDamage, dinfo, result);
+            Pawn_SwallowWhole attacker = dinfo.Instigator as Pawn_SwallowWhole;
+            if (attacker != null)
+            {
+                attacker.needs.food.CurLevel += 0.3f;
+                if (attacker.innerContainer.Count < 10)
+                {
+                    HealthUtility.DamageUntilDowned(pawn, true);
+                    attacker.TryAcceptThing(pawn);
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/PawnClasses/Pawn_SwallowWhole.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class Pawn_SwallowWhole : Pawn, IThingHolder
+    {
+
+        public ThingOwner innerContainer = null;
+
+        public Pawn_SwallowWhole()
+        {
+            //Constructor initializes the pawn container
+            this.innerContainer = new ThingOwner<Thing>(this, false, LookMode.Deep);
+         
+        }
+        public override void ExposeData()
+        {
+            //Save all the key variables so they work on game save / load
+            base.ExposeData();
+            Scribe_Deep.Look<ThingOwner>(ref this.innerContainer, "innerContainer", new object[] { this });
+         }
+        public new ThingOwner GetDirectlyHeldThings()
+        {
+            //Not used, included just in case something external calls it           
+            return this.innerContainer;
+        }
+
+        public new void GetChildHolders(List<IThingHolder> outChildren)
+        {
+            //Not used, included just in case something external calls it
+            ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, this.GetDirectlyHeldThings());
+        }
+
+        public virtual void EjectContents()
+        {
+            //Remove ingredients from the pawn container. 
+            this.innerContainer?.TryDropAll(this.InteractionCell, base.Map, ThingPlaceMode.Near, null, null);           
+        }
+
+        public void DestroyContents()
+        {
+            //Empties all containers and destroys contents
+            
+            if (this.innerContainer != null && this.innerContainer.Any)
+            {
+                this.innerContainer.ClearAndDestroyContents();
+            }
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            
+            EjectContents();           
+            base.Destroy(mode);
+        }
+
+        public bool TryAcceptThing(Thing thing, bool allowSpecialEffects = true)
+        {
+            if (!this.innerContainer.CanAcceptAnyOf(thing, true))
+            {
+                return false;
+            }
+            bool flag;
+            if (thing.holdingOwner != null)
+            {
+                thing.holdingOwner.TryTransferToContainer(thing, this.innerContainer, thing.stackCount, true);
+                flag = true;
+            }
+            else
+            {
+                flag = this.innerContainer.TryAdd(thing, true);
+            }
+            if (flag)
+            {
+                
+                return true;
+            }
+            return false;
+        }
+
+       
+
+
+    }
+}
