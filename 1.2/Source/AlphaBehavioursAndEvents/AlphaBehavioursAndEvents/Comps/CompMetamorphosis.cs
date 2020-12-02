9680a92fbd588c6295596e74d2d58acbafd4d523
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-12-02 20:03:49 +0100

    v2.11

--- 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompMetamorphosis.cs
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompMetamorphosis.cs
@@ -35,29 +35,33 @@ namespace AlphaBehavioursAndEvents
         public override void CompTickRare()
         {
             base.CompTickRare();
-            metamorphosisTick++;
-            //Only do it ater rareTicksInAYear * Props.timeInYears time has passed
-            if (metamorphosisTick> rareTicksInAYear * Props.timeInYears)
-            {
-                //Keep parent's faction, BUT only faction
-                Faction faction = this.parent.Faction;
-                           
-                PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.pawnToTurnInto), faction, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true, false, 1f, false, true, true, false, false);
-                Pawn pawn = PawnGenerator.GeneratePawn(request);
-                GenSpawn.Spawn(pawn, CellFinder.RandomClosewalkCellNear(parent.Position, parent.Map, 3, null), parent.Map, WipeMode.Vanish);
-                
-                //Produce some filth
-                for (int i = 0; i < 20; i++)
+            if (parent.Map != null) {
+                metamorphosisTick++;
+                //Only do it ater rareTicksInAYear * Props.timeInYears time has passed
+                if (metamorphosisTick > rareTicksInAYear * Props.timeInYears)
                 {
-                    IntVec3 c;
-                    CellFinder.TryFindRandomReachableCellNear(this.parent.Position, this.parent.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
-                    FilthMaker.TryMakeFilth(c, this.parent.Map, ThingDefOf.Filth_AmnioticFluid);
+                    //Keep parent's faction, BUT only faction
+                    Faction faction = this.parent.Faction;
+
+                    PawnGenerationRequest request = new PawnGenerationRequest(PawnKindDef.Named(Props.pawnToTurnInto), faction, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true, false, 1f, false, true, true, false, false);
+                    Pawn pawn = PawnGenerator.GeneratePawn(request);
+                    GenSpawn.Spawn(pawn, CellFinder.RandomClosewalkCellNear(parent.Position, parent.Map, 3, null), parent.Map, WipeMode.Vanish);
+
+                    //Produce some filth
+                    for (int i = 0; i < 20; i++)
+                    {
+                        IntVec3 c;
+                        CellFinder.TryFindRandomReachableCellNear(this.parent.Position, this.parent.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                        FilthMaker.TryMakeFilth(c, this.parent.Map, ThingDefOf.Filth_AmnioticFluid);
+                    }
+                    //Play insect spawn sound effect
+                    SoundDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(this.parent.Position, this.parent.Map, false));
+                    //Destroy the parent
+                    this.parent.Destroy();
                 }
-                //Play insect spawn sound effect
-                SoundDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(this.parent.Position, this.parent.Map, false));
-                //Destroy the parent
-                this.parent.Destroy();
+
             }
+            
         }
 
         public override string CompInspectStringExtra()
