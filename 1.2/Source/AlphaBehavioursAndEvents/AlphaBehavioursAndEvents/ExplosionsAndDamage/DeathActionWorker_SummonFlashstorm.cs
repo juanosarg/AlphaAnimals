09fb64b789f5a9abe5560404ffa6291a38ca1051
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-02-24 20:05:21 +0100

    v3.3

---
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/ExplosionsAndDamage/DeathActionWorker_SummonFlashstorm.cs
@@ -0,0 +1,24 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class DeathActionWorker_SummonFlashstorm : DeathActionWorker
+    {
+
+
+
+        public override void PawnDied(Corpse corpse)
+        {
+            if (corpse.Map != null) {
+                IncidentDef localDef = IncidentDef.Named("Flashstorm");
+                IncidentParms parms = StorytellerUtility.DefaultParmsNow(localDef.category, corpse.Map);
+                localDef.Worker.TryExecute(parms);
+            }
+            
+        }
+
+
+    }
+}
