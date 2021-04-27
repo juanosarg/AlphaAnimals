b39fd4d91f2b10069f4eb108fb8e32966588b83a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-04-27 16:52:27 +0200

    v3.34

--- 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Harmony/HarmonyPatchers.cs
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Harmony/HarmonyPatchers.cs
@@ -60,69 +60,7 @@ namespace AlphaBehavioursAndEvents
         }
     }
 
-    /*This Harmony Postfix changes terrain calculation for floating creatures
-   */
-    [HarmonyPatch(typeof(Verse.AI.Pawn_PathFollower))]
-    [HarmonyPatch("CostToMoveIntoCell")]
-    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(IntVec3) })]
-    public static class Pawn_PathFollower_CostToMoveIntoCell_Patch
-    {
-        
-        [HarmonyPostfix]
-        public static void MakeFloatingCreaturesGreatAgain(Pawn pawn, IntVec3 c, ref int __result)
-
-        {
-
-            if ((pawn.Map!=null) && (pawn.TryGetComp<CompFloating>() != null))
-            {
-                if (pawn.TryGetComp<CompFloating>().Props.isFloater)
-                {
-                    int num;
-                    if (c.x == pawn.Position.x || c.z == pawn.Position.z)
-                    {
-                        num = pawn.TicksPerMoveCardinal;
-                    }
-                    else
-                    {
-                        num = pawn.TicksPerMoveDiagonal;
-                    }
-                    TerrainDef terrainDef = pawn.Map.terrainGrid.TerrainAt(c);
-                    if (terrainDef == null)
-                    {
-                        num = 10000;
-                    } else if ((terrainDef.passability == Traversability.Impassable)&&!terrainDef.IsWater)
-                    {
-                        num = 10000;
-                    } else if (terrainDef.IsWater && !pawn.TryGetComp<CompFloating>().Props.canCrossWater)
-                    {
-                        num = 10000;
-                    }
-                    List<Thing> list = pawn.Map.thingGrid.ThingsListAt(c);
-                    for (int i = 0; i < list.Count; i++)
-                    {
-                        Thing thing = list[i];
-                        if (thing.def.passability == Traversability.Impassable)
-                        {
-                            num = 10000;
-                        }
-                       
-                        if (thing is Building_Door)
-                        {
-                            num += 45;
-                        }
-                    }
-                    
-                    __result = num;
-
-                }
-
-            }
-
-
-
-
-        }
-    }
+  
 
 
 
