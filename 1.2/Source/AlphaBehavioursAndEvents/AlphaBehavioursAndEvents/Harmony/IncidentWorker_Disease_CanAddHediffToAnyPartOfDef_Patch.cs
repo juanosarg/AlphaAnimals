cfcf140d660d8a722763c71e1e21ab25715e48ed
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-05-05 16:27:48 +0200

    v3.343

---
+++ 1.2/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Harmony/IncidentWorker_Disease_CanAddHediffToAnyPartOfDef_Patch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace AlphaBehavioursAndEvents
+{
+    /*This Harmony Postfix multiplies commonality of animals in the biome
+    */
+    [HarmonyPatch(typeof(IncidentWorker_Disease))]
+    [HarmonyPatch("CanAddHediffToAnyPartOfDef")]
+    public static class AlphaAnimals_IncidentWorker_Disease_CanAddHediffToAnyPartOfDef_Patch
+    {
+        [HarmonyPostfix]
+        public static void DontApplyDiseasesToMechsPlease(Pawn pawn, ref bool __result)
+
+        {
+
+            if (pawn.def.defName.Contains("AA_WaywardMobileAssembler"))
+            {
+                __result = false;
+
+            }
+
+
+        }
+    }
+}
