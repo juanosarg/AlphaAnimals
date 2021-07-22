58f2580c546401631356bd491eec3b45749dd766
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-22 08:44:03 +0200

    v3.6

--- 1.3/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Rituals/RitualRoleDryad.cs
+++ 1.3/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Rituals/RitualRoleDryad.cs
@@ -14,28 +14,40 @@ namespace AlphaBehavioursAndEvents
 			}
 		}
 
-		public override bool AppliesToPawn(Pawn p, out string reason, LordJob_Ritual ritual = null, RitualRoleAssignments assignments = null, Precept_Ritual precept = null)
+		public override bool AppliesToPawn(Pawn p, out string reason, LordJob_Ritual ritual = null, RitualRoleAssignments assignments = null, Precept_Ritual precept = null, bool skipReason = false)
 		{
+			reason = null;
 			if (!p.RaceProps.Animal)
 			{
-				reason = "MessageRitualRoleMustBeAnimal".Translate(base.LabelCap);
+
+				if (!skipReason)
+				{
+					reason = "MessageRitualRoleMustBeAnimal".Translate(base.LabelCap);
+				}
+				
 				return false;
 			}
 			if (p.def.defName != "AA_Dryad_Ocular")
 			{
-				reason = "AA_MessageRitualRoleNeedsDryad".Translate();
+				if (!skipReason)
+				{
+					reason = "AA_MessageRitualRoleNeedsDryad".Translate();
+				}
 				return false;
 			}
 			if (!p.Faction.IsPlayerSafe())
 			{
-				reason = "MessageRitualRoleMustBeColonist".Translate(base.Label);
+				if (!skipReason)
+				{
+					reason = "MessageRitualRoleMustBeColonist".Translate(base.Label);
+				}
 				return false;
 			}
 			reason = null;
 			return true;
 		}
 
-		public override bool AppliesToRole(Precept_Role role, out string reason, Precept_Ritual ritual = null, Pawn p = null)
+		public override bool AppliesToRole(Precept_Role role, out string reason, Precept_Ritual ritual = null, Pawn p = null, bool skipReason = false)
 		{
 			reason = null;
 			return false;
