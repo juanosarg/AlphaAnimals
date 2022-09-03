e306c24876194202c514333df454e1374ea72c96
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-03 18:17:12 +0200

    v3.9

---
+++ 1.3/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Abilities/Ability_OcularEruption.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Ability = VFECore.Abilities.Ability;
+using System.Linq;
+
+namespace AlphaBehavioursAndEvents
+{
+    [StaticConstructorOnStartup]
+    public class Ability_OcularEruption : Ability
+    {
+
+        public override void Cast(params GlobalTargetInfo[] targets)
+        {
+            base.Cast(targets);
+            foreach (GlobalTargetInfo target in targets)
+            {
+                Pawn pawn = target.Thing as Pawn;
+                IntRange numberOfEyes = new IntRange(4,7);
+
+                List<BodyPartRecord> pieces = pawn.health.hediffSet.GetNotMissingParts(BodyPartHeight.Undefined, BodyPartDepth.Undefined, null, null).ToList();
+
+                if (pieces!=null && pieces.Count > 0) {
+                    for (int i = 0; i < numberOfEyes.RandomInRange; i++)
+                    {
+                        BodyPartRecord part = pieces.RandomElement();
+                        var hediff = HediffMaker.MakeHediff(HediffDef.Named("AA_MalevolentEye"), pawn, part);
+                        pawn.health.AddHediff(hediff,part);
+                    }
+
+                }
+                
+                
+
+
+            }
+        }
+
+
+    }
+}
---
+++ 1.3/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/DefExtensions/Ability_Summon_Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class Ability_Summon_Extension : DefModExtension
+    {     
+        public PawnKindDef pawnToSpawn;
+        public bool enrage;
+        public bool playerFaction;
+        public FactionDef factionIfNotOfPlayer;
+        public int numberCreated; 
+    }
+}
