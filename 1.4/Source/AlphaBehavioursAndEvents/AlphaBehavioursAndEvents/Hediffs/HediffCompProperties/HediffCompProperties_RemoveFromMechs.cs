d15ba17e1e1867c282a319bd500ca861d1da4cb3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-23 09:27:54 +0200

    v4.104

---
+++ 1.4/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Hediffs/HediffCompProperties/HediffCompProperties_RemoveFromMechs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class HediffCompProperties_RemoveFromMechs : HediffCompProperties
+    {
+        
+
+        public HediffCompProperties_RemoveFromMechs()
+        {
+            this.compClass = typeof(HediffComp_RemoveFromMechs);
+        }
+    }
+}
