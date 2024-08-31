770333f3686fb476c603b491c52c5c11d801c374
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-08-31 15:29:58 +0200

    v5.2

---
+++ 1.5/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompProperties/CompProperties_Terraform.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using VFEInsectoids;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class CompProperties_Terraform : CompProperties_AOE
+    {
+        public TerrainDef terrainToLookFor;
+        public TerrainDef terrainToSet;
+        public CompProperties_Terraform()
+        {
+            this.compClass = typeof(CompTerraform);
+        }
+    }
+}
---
+++ 1.5/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/Comps/CompTerraform.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using VFEInsectoids;
+
+namespace AlphaBehavioursAndEvents
+{
+
+
+    public class CompTerraform : CompAOE_Cell
+    {
+        public CompProperties_Terraform Props => base.props as CompProperties_Terraform;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (!respawningAfterLoad)
+            {
+                parent.Map.terrainGrid.SetTerrain(parent.Position, Props.terrainToSet);
+            }
+        }
+
+        protected override bool CellValidator(IntVec3 cell)
+        {
+            var result = cell.GetTerrain(parent.Map) is TerrainDef terrain && terrain == Props.terrainToLookFor
+                && cell.GetEdifice(parent.Map) is null;
+          
+            return result;
+        }
+
+
+        protected override bool TryGetCell(List<IntVec3> cells, out IntVec3 cell)
+        {
+            cell = cells.OrderBy(x => x.DistanceTo(parent.Position)).FirstOrDefault();
+            return cell != default;
+        }
+
+        protected override void DoEffect(IntVec3 cell)
+        {
+            parent.Map.terrainGrid.SetTerrain(cell, Props.terrainToSet);
+        }
+    }
+}
