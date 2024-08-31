941cbe91b72aa0248d772a35345f5814fc5e9d7a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-08-31 11:22:55 +0200

    v5.2

---
+++ 1.5/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/MentalStates/MentalBreakWorker_PhotosensitiveExoskeleton.cs
@@ -0,0 +1,23 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class MentalBreakWorker_PhotosensitiveExoskeleton : MentalBreakWorker
+    {
+        public override bool BreakCanOccur(Pawn pawn)
+        {
+            if (!pawn.Spawned)
+            {
+                return false;
+            }
+            if (!base.BreakCanOccur(pawn))
+            {
+                return false;
+            }
+            return ThoughtWorker_PhotosensitiveExoskeleton.InSunlight(pawn);
+        }
+    }
+}
---
+++ 1.5/Source/AlphaBehavioursAndEvents/AlphaBehavioursAndEvents/ThoughtWorkers/ThoughtWorker_PhotosensitiveExoskeleton.cs
@@ -0,0 +1,34 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Noise;
+
+namespace AlphaBehavioursAndEvents
+{
+    public class ThoughtWorker_PhotosensitiveExoskeleton : ThoughtWorker
+    {
+      
+
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (!ModsConfig.BiotechActive)
+            {
+                return ThoughtState.Inactive;
+            }
+            if (p.genes == null || !p.genes?.HasActiveGene(DefDatabase<GeneDef>.GetNamedSilentFail("AA_Gene_PhotosensitiveExoskeleton"))==true || !InSunlight(p))
+            {
+                return ThoughtState.Inactive;
+            }
+            return ThoughtState.ActiveAtStage(0);
+        }
+
+        public static bool InSunlight(Pawn pawn)
+        {
+            if(SanguophageUtility.InSunlight(pawn.Position, pawn.Map))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
